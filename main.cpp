8d042da93d1c23f25fb7bde9ba7625b3ee8b0d4d
dzisinho <dzisbogdan26@gmail.com>
2025-03-03 11:39:31 +0200

    completed

--- main.cpp
+++ main.cpp
@@ -3,127 +3,86 @@
 
 using namespace std;
 
+template <typename T>
 struct FindMin {
-    int operator()(const vector<int>& nums) const {
-        int minVal = nums[0];
-        for (int num : nums) {
-            if (num < minVal) {
-                minVal = num;
-            }
-        }
+    T operator()(const vector<T>& nums) const {
+        T minVal = nums[0];
+        for (const T& num : nums)
+            if (num < minVal) minVal = num;
         return minVal;
     }
 };
 
+template <typename T>
 struct FindMax {
-    int operator()(const vector<int>& nums) const {
-        int maxVal = nums[0];
-        for (int num : nums) {
-            if (num > maxVal) {
-                maxVal = num;
-            }
-        }
+    T operator()(const vector<T>& nums) const {
+        T maxVal = nums[0];
+        for (const T& num : nums)
+            if (num > maxVal) maxVal = num;
         return maxVal;
     }
 };
 
-struct Descending {
-    void operator()(vector<int>& nums) const {
-        for (size_t i = 0; i < nums.size(); ++i) {
-            for (size_t j = i + 1; j < nums.size(); ++j) {
-                if (nums[i] < nums[j]) {
+template <typename T>
+struct Sort {
+    bool ascending;
+    Sort(bool asc) : ascending(asc) {}
+    void operator()(vector<T>& nums) const {
+        for (size_t i = 0; i < nums.size() - 1; ++i)
+            for (size_t j = i + 1; j < nums.size(); ++j)
+                if ((ascending && nums[i] > nums[j]) || (!ascending && nums[i] < nums[j]))
                     swap(nums[i], nums[j]);
-                }
-            }
-        }
     }
 };
 
-struct Ascending {
-    void operator()(vector<int>& nums) const {
-        for (size_t i = 0; i < nums.size(); ++i) {
-            for (size_t j = i + 1; j < nums.size(); ++j) {
-                if (nums[i] > nums[j]) {
-                    swap(nums[i], nums[j]);
-                }
-            }
-        }
+template <typename T>
+struct Modify {
+    T value;
+    Modify(T val) : value(val) {}
+    void operator()(vector<T>& nums, bool increase) const {
+        for (T& num : nums)
+            num += increase ? value : -value;
     }
 };
 
-struct Increase {
-    int value;
-    Increase(int val) : value(val) {}
-    void operator()(vector<int>& nums) const {
-        for (int& num : nums) {
-            num += value;
-        }
-    }
-};
-
-struct Decrease {
-    int value;
-    Decrease(int val) : value(val) {}
-    void operator()(vector<int>& nums) const {
-        for (int& num : nums) {
-            num -= value;
-        }
-    }
-};
-
+template <typename T>
 struct RemoveValue {
-    int value;
-    RemoveValue(int val) : value(val) {}
-    void operator()(vector<int>& nums) const {
-        nums.erase(remove(nums.begin(), nums.end(), value), nums.end());
+    T value;
+    RemoveValue(T val) : value(val) {}
+    void operator()(vector<T>& nums) const {
+        vector<T> temp;
+        for (const T& num : nums)
+            if (num != value) temp.push_back(num);
+        nums = temp;
     }
 };
+
+template <typename T>
+void printVector(const vector<T>& nums) {
+    for (const T& num : nums) cout << num << " ";
+    cout << endl;
+}
 
 int main() {
     vector<int> numbers = { 5, 3, 8, 1, 6, 8, 7 };
 
-    // Пошук мінімального значення
-    FindMin findMin;
-    cout << "Minimum value: " << findMin(numbers) << endl;
+    cout << "Min: " << FindMin<int>()(numbers) << endl;
+    cout << "Max: " << FindMax<int>()(numbers) << endl;
 
-    // Пошук максимального значення
-    FindMax findMax;
-    cout << "Maximum value: " << findMax(numbers) << endl;
+    Sort<int>(false)(numbers);
+    cout << "Sorted Descending: "; printVector(numbers);
 
-    // Сортування за спаданням
-    Descending descending;
-    descending(numbers);
-    cout << "Sorted in descending order: ";
-    for (int num : numbers) cout << num << " ";
-    cout << endl;
+    Sort<int>(true)(numbers);
+    cout << "Sorted Ascending: "; printVector(numbers);
 
-    // Сортування за зростанням
-    Ascending ascending;
-    ascending(numbers);
-    cout << "Sorted in ascending order: ";
-    for (int num : numbers) cout << num << " ";
-    cout << endl;
+    Modify<int>(2)(numbers, true);
+    cout << "Increased by 2: "; printVector(numbers);
 
-    // Збільшення значень на 2
-    Increase increase(2);
-    increase(numbers);
-    cout << "After increasing by 2: ";
-    for (int num : numbers) cout << num << " ";
-    cout << endl;
+    Modify<int>(1)(numbers, false);
+    cout << "Decreased by 1: "; printVector(numbers);
 
-    // Зменшення значень на 1
-    Decrease decrease(1);
-    decrease(numbers);
-    cout << "After decreasing by 1: ";
-    for (int num : numbers) cout << num << " ";
-    cout << endl;
-
-    // Видалення елементів, рівних 8
-    RemoveValue remove(8);
-    remove(numbers);
-    cout << "After removing 8: ";
-    for (int num : numbers) cout << num << " ";
-    cout << endl;
+    RemoveValue<int>(8)(numbers);
+    cout << "Removed 8: "; printVector(numbers);
 
     return 0;
 }
